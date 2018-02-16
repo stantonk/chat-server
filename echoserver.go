6ef17a14a6d829cd7a545444bb279162370ad98c
Kevin Stanton <stantonk@gmail.com>
2018-02-16 12:41:52 -0600

    working echo server

--- echoserver.go
+++ echoserver.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/gommon/log"
 	"time"
 	"net"
+	"fmt"
 )
 
 // Programming challenge: implement an echo server
@@ -12,6 +13,7 @@ func main() {
 	if err != nil {
 		// handle error
 	}
+	log.Info("Server started on", ln.Addr())
 
 	for {
 		conn, err := ln.Accept()
@@ -21,23 +23,29 @@ func main() {
 		go handleConnection(conn)
 	}
 }
+
 func handleConnection(conn net.Conn) {
+	log.Info("Connection established for ", conn.RemoteAddr())
 	//var buf = new([]byte)
 	//TODO: hmm, what's difference between new([]byte) and make([]byte, SIZE)?
-	var buf = make([]byte, 10)
+	var buf = make([]byte, 1024)
 	for {
 		time.Sleep(1e9)
-		nb, err := conn.Read(buf)
-		if err != nil {
-			log.Error("error:", err)
-			panic("oh well")
-		}
-		if nb > 0 {
-			log.Info(string(buf[:nb]))
+		// https://stackoverflow.com/questions/34717331/why-does-conn-read-write-nothing-into-a-byte-but-bufio-reader-readstring
+		if nb, err := conn.Read(buf); err != nil {
+			log.Warn("error:", err)
+			// TODO...could be connection closed, or something else...
+			break
 		} else {
-			log.Info("nothing received yet...")
+			log.Info(string(buf[:nb]))
+			// echo it back
+			response := []byte(fmt.Sprintf("%s: %s", "echo", buf[:nb]))
+			if _, err := conn.Write(response); err != nil {
+				log.Error("unable to write data back to client: ", err)
+				// probably dead connection?
+				break
+			}
 		}
-
 	}
-
+	log.Info("Done handling connection for ", conn.RemoteAddr())
 }
